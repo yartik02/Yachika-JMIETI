dad8d8afcbb23e915f43d44f40455cdee685eb44
yartik02 <kambojyartik@gmail.com>
2026-02-27 15:13:37 +0530

    removed extra backImg from ForgetPassword.jsx file

--- Client/src/pages/ForgetPassword.jsx
+++ Client/src/pages/ForgetPassword.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ForgetPassword.css";
 import { toast } from "react-toastify";
-import backIMg from "../assets/backgroundIMageComplaintForm.png"
 
 const API_BASE = `${import.meta.env.API_BASE_URL}/api/auth`;
 
